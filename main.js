5ed30a1ed2d0061abe7422cc039ca9f2123071fa
Alejandro Bay <alejandrobay23@gmail.com>
2022-12-01 02:43:46 -0500

    Update Ticket  Status Local Storage

--- main.js
+++ main.js
@@ -1,52 +1,91 @@
-document
-  .getElementById('ticketInputForm')
-  .addEventListener('submit', saveTicket)
-
 const inputSubject = document.getElementById('iSubImp')
 const inputDescription = document.getElementById('iDesImp')
-const inputSeverity = document.getElementById('iSevImp')
+const inputSeverity = document.getElementById('iSevInp')
 const inputResponsible = document.getElementById('iResImp')
 const inputId = chance.guid()
-const ticketList = document.getElementById('issue-ist')
+const ticketList = document.getElementById('issue-list')
 let tStatus = 'Open'
+// let statusColor = tStatus == 'Open' ? 'warning' : 'success'
 
 const fetchTickets = () => {
   const tickets = JSON.parse(localStorage.getItem('tickets'))
 
-  ticketList.innerHTML = ''
+  // ticketList.innerHTML = ''
 
-  for (i = 0; i < ticketList.length; i++) {
-    id = ticketList[i].id
-    subject = ticketList[i].subject
-    description = ticketList[i].description
-    severity = ticketList[i].severity
-    assignetTo = ticketList[i].assignetTo
-    ticketStatus = ticketList[i].ticketStatus
-    // statusColor = ticketList[i].statusColor
+  for (let i = 0; i < tickets.length; i++) {
+    const id = tickets[i].id
+    const subject = tickets[i].subject
+    const description = tickets[i].description
+    const severity = tickets[i].severity
+    const assignetTo = tickets[i].assignetTo
+    const ticketStatus = tickets[i].ticketStatus
+    let statusColor = ticketStatus == 'Open' ? 'warning' : 'success'
 
-    ticketList.innerHTML = `
-    <div class="container-sm bg-dark bg-opacity-50 p-4 my-2 rounded-3 ">
+    // const statusColor = 'success'
+
+    const content = `<div class="container-sm bg-dark bg-opacity-50 p-4 my-2 rounded-3 ">
               <h6> issue id:${id}</h6>
-              <span class="badge text-bg-${statusColor}">${ticketStatus}</span>
+              <span class="badge bg-${statusColor}">${ticketStatus}</span>
               <h3>${subject}</h3>
               <p>${description} </p>
               <p><i class="bi bi-clock-history">${severity}</i> <i class="bi bi-person-circle"> ${assignetTo}</i> </p>
-              <button type="submit" class="btn btn-warning">Close</button>
-              <button type="submit" class="btn btn-success">Delete</button>
-              
-    </div>`
+              <button type="submit" class="btn btn-warning" onclick="setStatusClosed('${id}')">Close</button>
+              </div>`
+    // <button type="submit" class="btn btn-danger" onclick="deleteIssue(${id})">Delete</button>
+    ticketList.insertAdjacentHTML('beforeend', content)
   }
 }
 
 const saveTicket = (e) => {
   const ticket = {
-    id: inputId.value,
+    id: inputId,
     subject: inputSubject.value,
     description: inputDescription.value,
     severity: inputSeverity.value,
     assignetTo: inputResponsible.value,
-    ticketStatus: tStatus.value,
+    ticketStatus: tStatus,
   }
-  console.log(ticket)
+  // console.log(ticket)
+
+  if (localStorage.getItem('tickets') === null) {
+    let tickets = []
+    tickets.push(ticket)
+    localStorage.setItem('tickets', JSON.stringify(tickets))
+  } else {
+    let tickets = JSON.parse(localStorage.getItem('tickets'))
+    tickets.push(ticket)
+    localStorage.setItem('tickets', JSON.stringify(tickets))
+  }
+
+  document.getElementById('ticketInputForm').reset()
+  fetchTickets()
+
   e.preventDefault()
 }
+
+const loadTickets = () => {
+  if (localStorage.getItem('tickets') !== null) {
+    fetchTickets()
+  }
+}
+
+const setStatusClosed = (id) => {
+  const tickets = JSON.parse(localStorage.getItem('tickets'))
+  tickets.forEach((ticket) => {
+    // console.log(ticket)
+    if (ticket.id === id) ticket.ticketStatus = 'Closed'
+    localStorage.setItem('tickets', JSON.stringify(tickets))
+  })
+  fetchTickets()
+}
+
+const deleteIssue = (id) => {
+  const tickets = JSON.parse(localStorage.getItem('tickets'))
+  tickets.filter((ticket) => ticket.id !== id)
+}
+
+loadTickets()
+
+document
+  .getElementById('ticketInputForm')
+  .addEventListener('submit', saveTicket)
